0cef1d4244d9973e7b23756ac6e189e543a93039
qvgk <45526481+QVGK@users.noreply.github.com>
2024-01-25 15:53:15 +0800

    2.0.7

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
+// Env
+import { config } from "dotenv";
+config();
+
+// Config Export
 export default {
-    allowedGuilds: ["1054956623182975068", "1193804554790260739"],
+    allowedGuilds: [`${process.env.GUILD_ID}`],
 };
